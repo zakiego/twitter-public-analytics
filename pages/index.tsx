52afe3072259d9c6a1f025aef7fe39e6f115ceca
Zakiego <belummikir@gmail.com>
2021-09-25 08:44:03 +0800

    Add head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { AtSymbolIcon } from "@heroicons/react/solid";
+import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
 import LoadingIcon from "~/components/icon/loading.png";
+import { useState } from "react";
 
 const Index = () => {
 	const [status, setStatus] = useState("");
@@ -90,48 +91,53 @@ const Index = () => {
 	};
 
 	return (
-		<div className="bg-soft min-h-screen relative text-primary">
-			<div className="max-w-3xl mx-auto">
-				<div className="max-w-4xl  mx-auto bg-gradient-to-bl from-[#2D5378] to-[#508698] h-36 absolute inset-0 rounded-b-3xl" />
-				<div className="pt-20 relative flex flex-col px-7">
-					{/* head */}
-					<div className="bg-white py-7 rounded-2xl text-center shadow-blues ">
-						<div className="text-2xl font-semibold">Pengikut Pertama</div>
-						<div className="mt-8 relative w-full flex-wrap items-stretch mb-3 px-11">
-							<span className="z-10 h-full text-center absolute flex items-center justify-center w-8 pl-3 py-3">
-								<AtSymbolIcon className="h-5 w-5" />
-							</span>
-							<input
-								onChange={inputHandler}
-								type="text"
-								className="transition-all duration-300 focus:ring-2  ring-[#2D5378] ring-opacity-50 rounded-2xl text-base px-3 py-3 relative border-2 border-tepi outline-none  w-full pl-10"
-							/>
-						</div>
-						<button
-							onClick={getData}
-							className="mt-2 bg-primary text-white font-semibold text-xs px-5 py-2 rounded-lg shadow-blues opacity-80 hover:opacity-100 transition-all"
-						>
-							Lihat
-						</button>
-					</div>
-
-					{/* content */}
-					<div className="mt-10 bg-white shadow-blues rounded-2xl text-center py-7">
-						{pageHandler()}
-						{/* footer */}
-						<div className="mt-9 text-primary/40 text-xs text-center">
-							{`Made with <3 by `}
-							<a
-								href="https://zakiego.my.id/"
-								className="border-b-[1px] border-hitam-1c/40 hover:border-hitam-1c/75 hover:text-hitam-1c/75 transition-all"
+		<>
+			<Head>
+				<title>Pengikut Pertama</title>
+			</Head>
+			<div className="bg-soft min-h-screen relative text-primary">
+				<div className="max-w-3xl mx-auto">
+					<div className="max-w-4xl  mx-auto bg-gradient-to-bl from-[#2D5378] to-[#508698] h-36 absolute inset-0 rounded-b-3xl" />
+					<div className="pt-20 relative flex flex-col px-7">
+						{/* head */}
+						<div className="bg-white py-7 rounded-2xl text-center shadow-blues ">
+							<div className="text-2xl font-semibold">Pengikut Pertama</div>
+							<div className="mt-8 relative w-full flex-wrap items-stretch mb-3 px-11">
+								<span className="z-10 h-full text-center absolute flex items-center justify-center w-8 pl-3 py-3">
+									<AtSymbolIcon className="h-5 w-5" />
+								</span>
+								<input
+									onChange={inputHandler}
+									type="text"
+									className="transition-all duration-300 focus:ring-2  ring-[#2D5378] ring-opacity-50 rounded-2xl text-base px-3 py-3 relative border-2 border-tepi outline-none  w-full pl-10"
+								/>
+							</div>
+							<button
+								onClick={getData}
+								className="mt-2 bg-primary text-white font-semibold text-xs px-5 py-2 rounded-lg shadow-blues opacity-80 hover:opacity-100 transition-all"
 							>
-								Zakiego
-							</a>
+								Lihat
+							</button>
+						</div>
+
+						{/* content */}
+						<div className="mt-10 bg-white shadow-blues rounded-2xl text-center py-7">
+							{pageHandler()}
+							{/* footer */}
+							<div className="mt-9 text-primary/40 text-xs text-center">
+								{`Made with <3 by `}
+								<a
+									href="https://zakiego.my.id/"
+									className="border-b-[1px] border-hitam-1c/40 hover:border-hitam-1c/75 hover:text-hitam-1c/75 transition-all"
+								>
+									Zakiego
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
